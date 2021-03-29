25733160b7b3ba58970bda960e76e65ebca5b050
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-03-29 07:10:22 +0700

    Add admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -133,8 +133,8 @@
       </nav>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <!-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Dashboard</h1>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <!-- <h1 class="h2">Dashboard</h1>
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -154,17 +154,17 @@
         <div class="row">
           <div class="card"></div>
         </div>
-
+        
         <h2>Section title</h2>
         <div class="table-responsive">
           <table class="table table-striped table-sm">
             <thead>
               <tr>
                 <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
+                <th>Gambar</th>
+                <th>Artikel</th>
+                <th>Kategori</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
