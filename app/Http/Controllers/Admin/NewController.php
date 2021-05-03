3ff99feba96400da810df44a9990a5474362ae62
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-05-03 07:43:35 +0700

    CRUD News

--- app/Http/Controllers/Admin/NewController.php
+++ app/Http/Controllers/Admin/NewController.php
@@ -62,9 +62,9 @@ class NewController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(News $news)
     {
-        //
+        return view('news.news-show', compact('$news'));
     }
 
     /**
